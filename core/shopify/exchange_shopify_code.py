1d938d8cbbadc46e640e1596c000ab03213eaa73
Jules LE BRIS <jls.lebris@gmail.com>
2026-06-11 21:39:44 +0200

    🔥 remove debug code

--- core/shopify/exchange_shopify_code.py
+++ core/shopify/exchange_shopify_code.py
@@ -12,21 +12,11 @@ def exchange_shopify_code(shop: str, params: dict) -> dict:
     if not shopify.Session.validate_params(params):
         raise ValueError("HMAC invalide.")
 
-    print(params)
-    print(type(params))
-    try:
-        access_token = session.request_token(params)
-    except Exception as e:
-        print(f"Error: {e}")
-        raise
-    print("access token ok")
+    access_token = session.request_token(params)
 
     shopify.ShopifyResource.activate_session(session)
-    print("session activated")
     shop_info = shopify.Shop.current()
-    print("shop info ok")
     shopify.ShopifyResource.clear_session()
-    print("session cleared")
     return {
         "access_token": access_token,
         "scopes": session.access_scopes,
